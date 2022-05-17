d884ca782d55336176fadbff7536fec67b6736ba
WhiteMIron <bmchhk13@gmail.com>
2022-05-17 22:27:53 +0900

    campground 스키마 및 모델 생성

---
+++ models/campground.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const CampgroundSchema = new Schema({
+    title: String,
+    price: String,
+    description: String,
+    location: String,
+});
+
+module.exports = mongoose.model('Campground', CampgroundSchema);
