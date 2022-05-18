3c6fc93f4b70957a053897e78d5dda229b517df4
WhiteMIron <bmchhk13@gmail.com>
2022-05-19 01:11:25 +0900

    image 추가

--- seeds/index.js
+++ seeds/index.js
@@ -19,11 +19,16 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({});
+
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            image: 'https://source.unsplash.com/collection/483251',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, nihil.',
+            price,
         });
         await camp.save();
     }
