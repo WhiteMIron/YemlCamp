b579b0cfa1d623d94c5aedb46ff7f1b6bb60c1fa
WhiteMIron <bmchhk13@gmail.com>
2022-05-24 01:05:08 +0900

    리뷰 delete 미들웨어 추가

--- models/campground.js
+++ models/campground.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
+import Review from './review.js';
 
 const CampgroundSchema = new Schema({
     title: String,
@@ -8,6 +9,16 @@ const CampgroundSchema = new Schema({
     description: String,
     location: String,
     reviews: [{ type: Schema.Types.ObjectId, ref: 'Review' }],
+});
+
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await Review.deleteMany({
+            id: {
+                $in: doc.reviews,
+            },
+        });
+    }
 });
 
 export default mongoose.model('Campground', CampgroundSchema);
