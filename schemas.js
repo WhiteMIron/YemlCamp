32f83a640cb1ed40bd2cdad626dfba4af4a21946
WhiteMIron <bmchhk13@gmail.com>
2022-05-23 23:14:33 +0900

    리뷰 모델 추가

--- schemas.js
+++ schemas.js
@@ -1,6 +1,6 @@
 import Joi from 'joi';
 
-export default campgroundSchema = Joi.object({
+export const campgroundSchema = Joi.object({
     campground: Joi.object({
         title: Joi.string().required,
         price: Joi.number().required().min(0),
